4871b37178e5056fe735d8a7e42efc45217e36cc
Alex-Charco <awcharco@utpl.edu.ec>
2025-01-10 17:38:36 -0500

    Corrección en el código para solucionar error

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,14 @@ import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
