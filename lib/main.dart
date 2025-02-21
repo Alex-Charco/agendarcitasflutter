f93c17f5ee45c0294b16ff39fe1aec3987bc9c61
Alex-Charco <awcharco@utpl.edu.ec>
2025-02-20 22:47:03 -0500

    Se comenta import de routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:agendarcitasflutter/routes/app_routes.dart';
+//import 'package:agendarcitasflutter/routes/app_routes.dart';
 import 'package:flutter/material.dart';
 //import 'package:firebase_core/firebase_core.dart';
 //import 'firebase/firebase_options.dart';
