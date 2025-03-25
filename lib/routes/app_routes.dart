4e7cf6594a7b50c1cec48d68a157a9de6b0f2894
Alex-Charco <awcharco@utpl.edu.ec>
2025-03-25 12:19:57 -0500

    Logica de login completada

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:agendarcitasflutter/views/login_view.dart';
+import 'package:agendarcitasflutter/views/home_view.dart';
 
 class AppRoutes {
   static const String login = '/login';
+  static const String home = '/home';
 
   static Route<dynamic> onGenerateRoute(RouteSettings settings) {
     final String routeName = settings.name ?? login;
@@ -10,6 +12,8 @@ class AppRoutes {
     switch (routeName) {
       case login:
         return MaterialPageRoute(builder: (_) => const LoginView());
+	  case home:
+        return MaterialPageRoute(builder: (_) => const HomeView());
       default:
         return MaterialPageRoute(builder: (_) => const LoginView());
     }
